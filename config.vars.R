19314a65fadb62472a331072136167363505cf79
Jose M. Perez Sanchez <jose@picofemto.com>
2015-12-21 17:23:58 -0500

    Update config.vars file

--- config.vars.R
+++ config.vars.R
@@ -1,4 +1,4 @@
-data.dir <- "C:\\Users\\JoseM\\Projects\\Capstone\\Data\\Coursera-SwiftKey\\final\\en_US"
+data.dir <- "C:\\Users\\jmperez\\Projects\\Capstone\\Data\\Coursera-SwiftKey\\final\\en_US"
 line.limit = 1000
 text.encoding = "unknown"
 text.language = substring(basename(data.dir), 1, 2)
