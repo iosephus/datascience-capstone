7b6f15efaf50b613b415e0c890474bff04f4f0f6
Jose M. Perez Sanchez <jose@picofemto.com>
2015-12-21 20:36:58 -0500

    Exploratory analysis: Add n-gram frequency plots for n = {1, 2, 3} using ggplot2

--- config.vars.R
+++ config.vars.R
@@ -1,5 +1,5 @@
 data.dir <- "C:\\Users\\jmperez\\Projects\\Capstone\\Data\\Coursera-SwiftKey\\final\\en_US"
-line.limit = 2000
+line.limit = 1000
 text.encoding = "unknown"
 text.language = substring(basename(data.dir), 1, 2)
 min.word.len = 1
